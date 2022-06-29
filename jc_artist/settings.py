6ebd7177e8475a1863b8edfd7b7fe27d0871f87a
Robn88 <robert.d.nicholas@gmail.com>
2022-06-29 11:22:01 +0000

    Add home app and templates

--- jc_artist/settings.py
+++ jc_artist/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'gallery',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +64,10 @@ ROOT_URLCONF = 'jc_artist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
