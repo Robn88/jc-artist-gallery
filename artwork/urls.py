ad6e65297d1a740570dcd29f3af60317096a9b5b
Robn88 <robert.d.nicholas@gmail.com>
2022-07-04 11:26:53 +0000

    FAdd artwork_detail ppage and view.

--- artwork/urls.py
+++ artwork/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_artwork, name='artwork')
+    path('', views.all_artwork, name='artwork'),
+    path('<artwork_id>/', views.artwork_detail, name='artwork_detail'),
 ]
--- artwork/views.py
+++ artwork/views.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Artwork
 
 # Create your views here.
 
 
 def all_artwork(request):
-    """ A view to return all of the artwork, including sorting and search queries"""
+    """ A view to return all of the artwork, including sorting and search queries """
 
     artwork = Artwork.objects.all()
 
@@ -14,3 +14,16 @@ def all_artwork(request):
     }
 
     return render(request, 'artwork/artwork.html', context)
+
+
+def artwork_detail(request, artwork_id):
+    """ A view to return an individual piece of artwork """
+
+    piece = get_object_or_404(Artwork, pk=artwork_id)
+    print(piece, artwork_id)
+
+    context = {
+        'piece': piece,
+    }
+
+    return render(request, 'artwork/artwork_detail.html', context)
