7ff5add6ead80de655648aa16345b92ca99cea52
Robn88 <robert.d.nicholas@gmail.com>
2022-07-18 10:42:53 +0000

    Add urls, views and template for checkout

--- jc_artist/settings.py
+++ jc_artist/settings.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     'artwork',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -63,6 +66,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'jc_artist.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -80,6 +85,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
