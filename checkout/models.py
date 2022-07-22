eb4a8c112350e91642cdcc86489e49136b48e69a
Robn88 <robert.d.nicholas@gmail.com>
2022-07-22 11:03:01 +0000

    Add profiles app

--- checkout/models.py
+++ checkout/models.py
@@ -3,13 +3,18 @@ import uuid
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+
 from artwork.models import Artwork
+from profiles.models import UserProfile
 
 from django_countries.fields import CountryField
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -20,8 +25,10 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    order_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2,
+                                      null=False, default=0)
 
     def _generate_order_number(self):
         """
--- jc_artist/settings.py
+++ jc_artist/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'artwork',
     'bag',
     'checkout',
+    'profiles',
 
     # Other
     'crispy_forms',
