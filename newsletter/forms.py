945aac4972014bcdead5d7beb1f7ef8ec8826b93
Robn88 <robert.d.nicholas@gmail.com>
2022-07-27 12:25:41 +0000

    Add urls,form and views to newsletter app

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -1,10 +1,12 @@
 from django import forms
-# from .models import NewsletterReceiver
+from .models import NewsletterReceiver
 
 
-# class NewsletterForm(forms.ModelForm):
-#     class Meta:
-#         model = NewsletterReceiver
+class NewsletterForm(forms.ModelForm):
+    class Meta:
+        model = NewsletterReceiver
+        fields = ['email']
 
-#         fields = ('email')
-#         return Email
+        def clean_email(self):
+            email = self.cleaned_data.get('email')
+            return Email
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
+from .forms import NewsletterForm
+from .models import NewsletterReceiver
+
+
+def newsletter_signup(request):
+
+    form = NewsletterForm(request.POST or None)
+
+    if form.is_valid():
+        instance = form.save(commit=False)
+        if NewsletterReceiver.objects.filter(email=instance.email).exists():
+            messages.error(request, 'Sorry, this email address is already \
+                            signed up to the newsletter!')
+        else:
+            instance.save()
+
+    template = 'newsletter/newsletter.html'
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
