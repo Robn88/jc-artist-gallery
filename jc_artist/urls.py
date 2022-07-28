bd938cdf0eff0a1af0b5b6c3ac943c1eed993f6d
Robn88 <robert.d.nicholas@gmail.com>
2022-07-28 15:26:09 +0000

    Fix error with handler404 i nurls.py

--- jc_artist/urls.py
+++ jc_artist/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('newsletter/', include('newsletter.urls')),
     path('contact/', include('contact.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-handler404 = 'boutique_ado.views.handler404'
+handler404 = 'jc_artist.views.handler404'
